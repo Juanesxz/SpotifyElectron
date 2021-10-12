be97596fbf39e0ba8784559572b5fc93f60f2944
Juanesxz <juaneswilli@hotmail.com>
2021-10-12 15:26:03 -0500

    grandes cambios 2

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,6 +3,7 @@ import firebase from "firebase/app";
 const db = firebase.firestore(firebaseApp);
 
 
+
 export async function isUserAdmin(uid) {
   const response = await db.collection("admins").doc(uid).get();
   return response.exists;
