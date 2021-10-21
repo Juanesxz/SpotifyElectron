5d40b27095f283e7a7c003a220a9955fd7937aa8
Juanesxz <juaneswilli@hotmail.com>
2021-10-20 23:38:01 -0500

    email y password

--- src/components/Settings/UserEmail.js
+++ src/components/Settings/UserEmail.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Button } from "semantic-ui-react";
-
+import React, { useState } from "react";
+import { Button, Form, Input, Icon } from "semantic-ui-react";
 
 export default function UserEmail(props) {
     const { user, setShowModal, setTitleModal, setContentModal } = props;
 
     const onEdit = () => {
-        console.log("Editando email")
-    }
+        setTitleModal("Actualizar email");
+        setContentModal(
+            <ChangeEmailForm email={user.email} setShowModal={setShowModal} />
+        );
+        setShowModal(true);
+    };
 
     return (
         <div className="user-email">
@@ -16,5 +19,47 @@ export default function UserEmail(props) {
                 actualizar
             </Button>
         </div>
-    )
+    );
+}
+
+function ChangeEmailForm(props) {
+    const { email, setShowModal } = props;
+    const [formData, setFormData] = useState({ email: "", password: "" });
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const onSubmit = () => {
+        console.log("Formulario enviado");
+
+        setShowModal(false);
+    };
+
+    return (
+        <Form onSubmit={onSubmit}>
+            <Form.Field>
+                <Input
+                    defaultValue={email}
+                    type="text"
+                    onChange={e => {
+                        return setFormData({ ...formData, email: e.target.value });
+                    }}
+                />
+            </Form.Field>
+            <Form.Field>
+                <Input
+                    placeholder="Contraseña"
+                    type={showPassword ? "text" : "password"}
+                    onChange={e => setFormData({ ...formData, password: e.target.value })}
+                    icon={
+                        <Icon
+                            name={showPassword ? "eye slash outline" : "eye"}
+                            link
+                            onClick={() => setShowPassword(!showPassword)}
+                        />
+                    }
+                />
+            </Form.Field>
+            <Button type="submit">Actualizar Contraseña</Button>
+        </Form>
+    );
 }
