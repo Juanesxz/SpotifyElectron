8160fc20456f185202ee56f897f4c4b14e195e36
Juanesxz <juaneswilli@hotmail.com>
2021-10-22 00:28:54 -0500

    Vereficacion y cambio de email

--- src/components/Settings/UserEmail.js
+++ src/components/Settings/UserEmail.js
@@ -1,65 +1,99 @@
 import React, { useState } from "react";
 import { Button, Form, Input, Icon } from "semantic-ui-react";
+import { toast } from "react-toastify";
+import { reauthenticate } from "../../utils/Api";
+import alerErrors from "../../utils/AlertErrors";
+import firebase from "../../utils/Firebase";
+import "firebase/auth";
 
 export default function UserEmail(props) {
-    const { user, setShowModal, setTitleModal, setContentModal } = props;
+  const { user, setShowModal, setTitleModal, setContentModal } = props;
 
-    const onEdit = () => {
-        setTitleModal("Actualizar email");
-        setContentModal(
-            <ChangeEmailForm email={user.email} setShowModal={setShowModal} />
-        );
-        setShowModal(true);
-    };
-
-    return (
-        <div className="user-email">
-            <h3>Email: {user.email}</h3>
-            <Button circular onClick={onEdit}>
-                actualizar
-            </Button>
-        </div>
+  const onEdit = () => {
+    setTitleModal("Actualizar email");
+    setContentModal(
+      <ChangeEmailForm email={user.email} setShowModal={setShowModal} />
     );
+    setShowModal(true);
+  };
+
+  return (
+    <div className="user-email">
+      <h3>Email: {user.email}</h3>
+      <Button circular onClick={onEdit}>
+        actualizar
+      </Button>
+    </div>
+  );
 }
 
 function ChangeEmailForm(props) {
-    const { email, setShowModal } = props;
-    const [formData, setFormData] = useState({ email: "", password: "" });
+  const { email, setShowModal } = props;
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const onSubmit = () => {
+    if (!formData.email) {
+      toast.warning("El email es el mismo");
+    } else {
+      setIsLoading(true);
+      reauthenticate(formData.password)
+        .then(() => {
+          const currentUser = firebase.auth().currentUser;
 
-    const [showPassword, setShowPassword] = useState(false);
+          currentUser
+            .updateEmail(formData.email)
+            .then(() => {
+              toast.success("Email Actualizado.");
+              setIsLoading(false);
+              setShowModal(false);
+              currentUser.sendEmailVerification().then(() => {
+                firebase.auth().signOut();
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+              alerErrors(err?.code);
+              setIsLoading(false);
+            });
+        })
+        .catch((err) => {
+          alerErrors(err?.code);
+          setIsLoading(false);
+        });
+    }
+  };
 
-    const onSubmit = () => {
-        console.log("Formulario enviado");
-        console.log(formData);
-        setShowModal(false);
-    };
-
-    return (
-        <Form onSubmit={onSubmit}>
-            <Form.Field>
-                <Input
-                    defaultValue={email}
-                    type="text"
-                    onChange={e => {
-                        return setFormData({ ...formData, email: e.target.value });
-                    }}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                    placeholder="Contraseña"
-                    type={showPassword ? "text" : "password"}
-                    onChange={e => setFormData({ ...formData, password: e.target.value })}
-                    icon={
-                        <Icon
-                            name={showPassword ? "eye slash outline" : "eye"}
-                            link
-                            onClick={() => setShowPassword(!showPassword)}
-                        />
-                    }
-                />
-            </Form.Field>
-            <Button type="submit">Actualizar Contraseña</Button>
-        </Form>
-    );
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Field>
+        <Input
+          defaultValue={email}
+          type="text"
+          onChange={(e) => {
+            return setFormData({ ...formData, email: e.target.value });
+          }}
+        />
+      </Form.Field>
+      <Form.Field>
+        <Input
+          placeholder="Contraseña"
+          type={showPassword ? "text" : "password"}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+          icon={
+            <Icon
+              name={showPassword ? "eye slash outline" : "eye"}
+              link
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          }
+        />
+      </Form.Field>
+      <Button type="submit" loading={isLoading}>
+        Actualizar email
+      </Button>
+    </Form>
+  );
 }
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -2,9 +2,17 @@ import firebaseApp from "./Firebase";
 import firebase from "firebase/app";
 const db = firebase.firestore(firebaseApp);
 
-
-
 export async function isUserAdmin(uid) {
   const response = await db.collection("admin").doc(uid).get();
   return response.exists;
 }
+
+export const reauthenticate = (password) => {
+  const user = firebase.auth().currentUser;
+
+  const credentials = firebase.auth.EmailAuthProvider.credential(
+    user.email,
+    password
+  );
+  return user.reauthenticateWithCredential(credentials);
+};
