5c2bc55805af5dede299f5a6338faa81b55211ac
Juanesxz <juaneswilli@hotmail.com>
2021-10-25 00:29:36 -0500

    Habilitando la subida de imagenes para el artista

--- src/components/Artists/AddArtistForm/AddArtistForm.js
+++ src/components/Artists/AddArtistForm/AddArtistForm.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Form, Input, Button, Image } from "semantic-ui-react";
-import "./AddArtistForm";
+import { useDropzone } from "react-dropzone";
+import NoImage from "../../../assets/png/no-image.png";
+import "./AddArtistForm.scss"
 
-export default function AddArtistForm() {
+export default function AddArtistForm(props) {
+    const { setShowModal } = props;
+    const [banner, setBanner] = useState(null);
+    const [file, setFile] = useState(null);
+
+    const onDrop = useCallback((acceptedFiles) => {
+        console.log(acceptedFiles);
+    });
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: "image/jpeg, image/png",
+        noKeyboard: true,
+        onDrop,
+    });
 
     const onSubmit = () => {
         console.log("Creando Nuevo Artista");
-    }
+        setShowModal(false);
+    };
 
     return (
         <Form className="add-artist-form" onSubmit={onSubmit}>
             <Form.Field className="artist-banner">
-                <input type="file" />
+                <div {...getRootProps()} className="banner" />
+                <input {...getInputProps()} />
             </Form.Field>
             <Form.Field className="artist-avatar">
                 <div>Avatar</div>
@@ -21,6 +38,5 @@ export default function AddArtistForm() {
             </Form.Field>
             <Button type="submit">Crear artista</Button>
         </Form>
-
     );
 }
